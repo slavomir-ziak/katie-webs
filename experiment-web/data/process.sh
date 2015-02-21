9bba9f990092e76055f7f9f639c9ee98434bf69b
Slavomir Ziak <slavomir.ziak@gmail.com>
2015-02-21 11:01:25 +0100

    [TASK] new data downloaded, process script parametrization

--- experiment-web/data/process.sh
+++ experiment-web/data/process.sh
@@ -5,7 +5,9 @@ rm katie/*
 for f in $(grep -r nalada _original/ | cut -d ":" -f1 | sort | uniq) ; do cp $f katie/$RANDOM; done
 
 # has to return 1 line
-for f in $(find katie -type f) ; do cat $f | head -1; done | sort | uniq > data.csv
+owner=martonova
 
-for f in $(find katie -type f) ; do cat $f | tail -1 >> data.csv; done 
+for f in $(find $owner -type f) ; do cat $f | head -1; done | sort | uniq > ${owner}_data.csv
+
+for f in $(find $owner -type f) ; do cat $f | tail -1 >> ${owner}_data.csv; done 
 
